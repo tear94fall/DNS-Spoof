e8befdc73a55393c057815da60adaf61be903c74
tear94fall <tear94fall@naver.com>
2019-11-11 16:21:10 +0900

    ip header, udp header add

--- main.cpp
+++ main.cpp
@@ -1,3 +1,9 @@
+
+/*
+./main ens33 www.google.com 192.168.218.147 192.168.218.148 192.168.218.2
+           intf      도메인     attacker's ip    victim's ip   gateway's ip
+*/
+
 #include <stdio.h>
 #include <string.h>
 #include <string>
@@ -8,17 +14,46 @@
 
 #include <pcap.h>
 
+
+
+#include <linux/if_ether.h>
+
+typedef struct ip_address {
+	u_char byte1;
+	u_char byte2;
+	u_char byte3;
+	u_char byte4;
+}ip_address;
+
+typedef struct ip_header{
+	u_char ver_ihl; // Version (4 bits) + Internet header length (4 bits)  
+	u_char tos; // Type of service   
+	u_short tlen; // Total length   
+	u_short identification; // Identification  
+	u_short flags_fo; // Flags (3 bits) + Fragment offset (13 bits)  
+	u_char ttl; // Time to live  
+	u_char proto; // Protocol  
+	u_short crc; // Header checksum  
+	ip_address saddr; // Source address  
+	ip_address daddr; // Destination address  
+	u_int op_pad; // Option + Padding  
+}ip_header;
+
+typedef struct udp_header {
+	u_short sport;   // Source port  
+	u_short dport;   // Destination port  
+	u_short len;   // Datagram length  
+	u_short crc;   // Checksum  
+}udp_header;
+
 typedef struct DnsMessageHeader{
     short ID;
 
-     //Start of second row which is broken into 2 bytes
     unsigned char RD : 1;
     unsigned char TC : 1;
     unsigned char AA : 1;
     unsigned char OPCODE : 4;
     unsigned char QR : 1;
-
-    // Start of Second byte needed for row 2
 
     unsigned char RCODE : 4;
     unsigned char CD : 1;
@@ -26,14 +61,80 @@ typedef struct DnsMessageHeader{
     unsigned char Z : 1;
     unsigned char RA : 1;
 
-    // End Second row
     short QDCNT;
     short ANCNT;
     short NSCNT;
     short ARCNT;
 } dns_header;
 
+void packet_handler(u_char *param,const struct pcap_pkthdr *header, const u_char *pkt_data);
+void print_packet_data(const struct pcap_pkthdr *header, const u_char *pkt_data);
+
+
+int main(int argc, char **argv) {
+    struct bpf_program fcode;
+    bpf_u_int32 mask;
+    pcap_t *adhandle;
+    char errbuf[PCAP_ERRBUF_SIZE];
+    pcap_if_t *alldevs;
+    pcap_if_t *d;
+    struct pcap_addr *a;
+    int i = 0;
+    int no;
+
+    const char * filter = "port 53 and ((udp and (not udp[10] & 128 = 0)) or (tcp and (not tcp[((tcp[12] & 0xf0) >> 2) + 2] & 128 = 0)))";
+
+    if (pcap_findalldevs(&alldevs, errbuf) < 0) {
+        printf("pcap_findalldevs error\n");
+        return 1;
+    }
+
+    for (d=alldevs; d; d=d->next) {
+        printf("%d :  %s\n", ++i, (d->description)?(d->description):(d->name));
+    }
+
+	printf("Enter the interface number you would like to sniff : ");
+	scanf("%d", &no);
+
+    if (!(no > 0 && no <= i)) {
+        printf("number error\n");
+        return 2;
+    }
+
+    for (d=alldevs, i=0; d; d=d->next) {
+        if (no == ++i){
+            break;
+        }
+    }
+
+    if (!(adhandle= pcap_open_live(d->name, 65536, 1, 1000, errbuf))) {
+        printf("pcap_open_live error %s\n", d->name);
+        pcap_freealldevs(alldevs);
+        return 3;
+    }
+
+	if (pcap_compile(adhandle, &fcode, filter, 1, mask) == -1) {
+		fprintf(stderr, "ERROR: %s\n", pcap_geterr(adhandle));
+        return 4;
+	}
+
+	if (pcap_setfilter(adhandle, &fcode) == -1) {
+		fprintf(stderr, "ERROR: %s\n", pcap_geterr(adhandle));
+		return 5;
+	}
+
+    pcap_freealldevs(alldevs);
+    pcap_loop(adhandle, 0, packet_handler, NULL);
+    pcap_close(adhandle);
+
+    return 0;
+}
+
 void packet_handler(u_char *param,const struct pcap_pkthdr *header, const u_char *pkt_data) {
+    print_packet_data(header, pkt_data);
+}
+
+void print_packet_data(const struct pcap_pkthdr *header, const u_char *pkt_data){
     dns_header *dns_hdr;
     dns_hdr = (dns_header*)(pkt_data + 42);
     printf("DNS Header\n");
@@ -65,7 +166,7 @@ void packet_handler(u_char *param,const struct pcap_pkthdr *header, const u_char
 	for (i = 0; i < Size; i++)	{
 		c = pkt_data[i];
         if(i!=0&&i%8==0){
-            printf("    ");
+            printf("  ");
         }
 
         if(i!=0&&i%16==0){
@@ -73,67 +174,4 @@ void packet_handler(u_char *param,const struct pcap_pkthdr *header, const u_char
         }
         printf("%.2x ", (unsigned int)c);
 	}printf("\n\n");
-}
-
-int main(int argc, char **argv) {
-    struct bpf_program fcode;
-    bpf_u_int32 mask;
-    pcap_t *adhandle;
-    char errbuf[PCAP_ERRBUF_SIZE];
-    pcap_if_t *alldevs;
-    pcap_if_t *d;
-    struct pcap_addr *a;
-    int i = 0;
-    int no;
-
-    const char * filter = "port 53 and ((udp and (not udp[10] & 128 = 0)) or (tcp and (not tcp[((tcp[12] & 0xf0) >> 2) + 2] & 128 = 0)))";
-
-    if (pcap_findalldevs(&alldevs, errbuf) < 0) {
-        printf("pcap_findalldevs error\n");
-        return 1;
-    }
-
-    for (d=alldevs; d; d=d->next) {
-        printf("%d :  %s\n", ++i, (d->description)?(d->description):(d->name));
-    }
-
-	printf("Enter the interface number you would like to sniff : ");
-	scanf("%d", &no);
- 
-    printf("=====================================================\n");
-
-    if (!(no > 0 && no <= i)) {
-        printf("number error\n");
-        return 1;
-    }
-
-    for (d=alldevs, i=0; d; d=d->next) {
-        if (no == ++i){
-            break;
-        }
-    }
-
-    if (!(adhandle= pcap_open_live(d->name, 65536, 1, 1000, errbuf))) {
-        printf("pcap_open_live error %s\n", d->name);
-        pcap_freealldevs(alldevs);
-        return -1;
-    }
-
-	if (pcap_compile(adhandle, &fcode, filter, 1, mask) == -1) {
-		fprintf(stderr, "ERROR: %s\n", pcap_geterr(adhandle));
-        return -1;
-	}
-
-	if (pcap_setfilter(adhandle, &fcode) == -1) {
-		fprintf(stderr, "ERROR: %s\n", pcap_geterr(adhandle));
-		return -2;
-	}
-
-    pcap_freealldevs(alldevs);
-
-    pcap_loop(adhandle, 0, packet_handler, NULL);
-
-    pcap_close(adhandle);
-
-    return 0;
 }
