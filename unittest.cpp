90a3cae00df3bff22f6c63ca3f47430b291bb4f4
tear94fall <tear94fall@naver.com>
2019-12-03 14:15:18 +0900

    1.유닛 테스트 수정

--- unittest.cpp
+++ unittest.cpp
@@ -6,12 +6,14 @@
 #include "packet_handler.cpp"
 
 TEST(attack_test, capture_test) {
-    packet_handle pkt_hnd = packet_handle();
+    packet_handle *pkt_hnd = (packet_handle*)malloc(sizeof(packet_handle));
     
     char file_name[128] = "info";
-    pkt_hnd.attack_info_file=file_name;
+    pkt_hnd->attack_info_file=file_name;
 
-    EXPECT_EQ(0, pkt_hnd.packet_capture_start()); 
+    EXPECT_EQ(0, pkt_hnd->packet_capture_start()); 
+
+    delete pkt_hnd;
 }
 
 int main(int argc, char **argv) {
