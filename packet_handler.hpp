cd069cecc861545cfb1dbd7b094ab215d6d97e25
tear94fall <tear94fall@naver.com>
2019-12-03 11:46:25 +0900

    클래스 파일 수정

--- packet_handler.hpp
+++ packet_handler.hpp
@@ -4,33 +4,17 @@
 #include <pcap.h>
 #include "protocol.hpp"
 
-namespace packet{
-    namespace packet_header{
-        class packets{
-            public:
-            ether_header *eth_hdr;
-            ip_header *ip_hdr;
-            udp_header *udp_hdr;
-            dns_header *dns_hdr;
-        };
-    }
+class packets{
+    public:
+    ether_header *eth_hdr;
+    ip_header *ip_hdr;
+    udp_header *udp_hdr;
+    dns_header *dns_hdr;
+};
 
-    namespace packet_hndlr{
-        class packet_hndlr:public packet_header::packets{
-        public:
-            struct pcap_pkthdr header;
-            const unsigned char* pkt_data = NULL;
-            int select_interface_number;
-            char *select_interface_name;
-
-            char domain[1024];
-            char fake_webserver_ip[16];
-            char *my_ip;
-
-        private:
-            void packet_handler(unsigned char *param, const struct pcap_pkthdr *header, const unsigned char *pkt_data);
-        };
-    }
-}
+class packet_handle : public packets{
+    public:
+    void packet_handler(unsigned char *param, const struct pcap_pkthdr *header, const unsigned char *pkt_data);
+};
 
 #endif
